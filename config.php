d5bec535fb6ce2b28728dfc78ff826184daf4c3c
Lucas Combs <l-combs@hotmail.com>
2017-11-08 22:35:35 -0500

    First Commit. Simple project with just a little HTML and CSS.

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+
+// Connecting to the MySQL database
+	$username = "combsl10";
+	$password = "sPIe3lar";
+	$hostname = "csweb.hh.nku.edu";
+	$schema = "db_fall17_combsl10";
+	$database = new PDO('mysql:host=' . $hostname . ';dbname=' . $schema,$username,$password);
+
+//Auto load
+function autoloader($class){
+	include 'class.' . $class . '.php';
+}
+
+spl_autoload_register('autoloader');
+/*
+// Start the session
+session_start();
+
+$current_url = basename($_SERVER['REQUEST_URI']);
+
+// if customerID is not set in the session and current URL not login.php redirect to login page
+if (!isset($_SESSION["customerID"]) && $current_url != 'login.php') {
+    header("Location: login.php");
+}
+
+// Else if session key customerID is set get $customer from the database
+elseif (isset($_SESSION["customerID"])) {
+	
+	
+	$sql = file_get_contents('sql/getCustomer.sql');
+	$params = array(
+		'customerid' => $_SESSION["customerID"]
+	);
+	$statement = $database->prepare($sql);
+	$statement->execute($params);
+	$customers = $statement->fetchAll(PDO::FETCH_ASSOC);
+	$customer = new Customer($customers[0]['customerid'],$database);
+
+}*/
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// Include a configuration file with the database connection
+include('config.php');
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+  	<title>Home</title>
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<!--[if lt IE 9]>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  	<![endif]-->
+</head>
+<body>
+		<div id="title-bar">
+            <p>Title bar</p>
+        </div>
+        <div id="nav-bar">
+            <p>Nav bar</p>
+        </div>
+        <div id="content">
+            <p>content</p>
+        </div>
+</body>
+</html>
