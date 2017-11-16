428f6ffb774d840d2c0db72578fa4a3b0c8e75d0
Lucas Combs <l-combs@hotmail.com>
2017-11-16 02:09:48 -0500

    JavaScript for login/sign-up now worksgit add *git add *git add *

--- login.php
+++ login.php
@@ -51,22 +51,123 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			</div>
         </div>
         <div id="content">
-			<div id="left">
-			<h1>Login</h1>
-			<form method="POST">
-				<input type="text" name="email" placeholder="Email" /><br/>
-				<input type="password" name="password" placeholder="Password" /><br/>
-				<input type="submit" value="Log In" />
-			</form>
-			</div>
-			<div id="right">
-				<h1>Sign Up</h1>
-				<form method="POST">
-					<input type="text" name="email" placeholder="Email" /><br/>
-					<input type="password" name="password" placeholder="Password" /><br/>
-					<input type="submit" value="Sign Up" />
+			<div id="formArea">
+				<h1 id="formTitle">Login</h1>
+				<form id="loginForm" method="POST">
+					<input id="formAction" type="text" name="action" hidden/>
+					<table>
+						<tbody id="formTableBody">
+						<tr class="signup">
+							<td>
+								<label>Name:</label>
+							</td>
+							<td colspan="3">
+								<input type="text" name="first" placeholder="John" />
+							</td>
+							<td colspan="3">
+								<input type="text" name="last" placeholder="Doe" />
+							</td>
+						</tr>
+						<tr>
+							<td><label>Email:</label></td>
+							<td colspan=6><input type="email" name="email" placeholder="handle@domain.com" /></td>
+						</tr>
+						<tr>
+							<td><label>Password:</label></td>
+							<td colspan=6><input type="password" name="password" placeholder="*****" /></td>
+						</tr>
+						<tr class="signup">
+							<td><label>Confirm Password:</label></td>
+							<td colspan=6><input type="password" name="passwordConfirm" placeholder="*****" /></td>
+						</tr>
+						<tr class="signup">
+							<td><label>Address 1:</label></td>
+							<td colspan=6><input type="text" name="address1" placeholder="100 West Main Street" /></td>
+						</tr>
+						<tr class="signup">
+							<td><label>Address 2:</label></td>
+							<td colspan=6><input type="text" name="address2" placeholder="Apartment #4E" /></td>
+						</tr>
+						<tr class="signup">
+							<td><label>City, State, Zip: </label></td>
+							<td colspan=4><input type="text" name="city" placeholder="Louisville" /></td>
+							<td colspan=1><input type="text" name="state" placeholder="KY" pattern="[A-Za-z]{2}" size=2/></td>
+							<td colspan=1><input type="text" name="zip" placeholder="40272" size=10 /></td>
+						</tr>
+						<tr>
+							<td colspan=4>
+								<input id="submitButton" type="submit" value="Login" />
+							</td>
+							<td colspan=4>
+								<input id="signupButton" type="button" value="Sign-Up!" />
+							</td>
+						</tr>
+						</tbody>
+					</table>
 				</form>
 			</div>
         </div>
+	<script type="text/javascript">
+	//Define all element variables
+	var children = document.querySelector("#formTableBody").children;
+	var signupButton = document.querySelector("#signupButton");
+	var submitButton = document.querySelector("#submitButton");
+	var loginForm = document.querySelector("#loginForm");
+	var formTitle = document.querySelector("#formTitle");
+	var formAction = document.querySelector("#formAction");
+	
+	//Start off in login mode
+	formAction.textContent = 'login';	
+	
+	//Listen for when someone wants to switch to sign up (Or go back)
+	signupButton.addEventListener("click",signupButtonClick,false);
+
+	//helper function, returns true if elem has klass (Class is reserved word)
+	function hasClass( elem, klass ) {
+     return (" " + elem.classList + " " ).indexOf( " "+klass+" " ) > -1;
+	}
+	
+	//Called everytime button it clicked
+	function signupButtonClick(){
+		
+		//Loop through all children
+		for(var i = 0; i < children.length; i++){
+			//current child
+			var child = children[i];
+			
+			if(hasClass(child,'signup')){
+				//Flip between inherit and none
+				child.style.display = child.style.display==='inherit'? 'none' : 'inherit';
+			}else{
+				//This helps the boxes that weren't orginally hidden re-size
+				repaint(child);
+			}
+			
+		}
+		
+		//Flip values to reflect changed form
+		if(formAction.textContent === 'login'){
+		   formAction.textContent = 'signup';
+		   formTitle.textContent = 'Sign-Up';
+		   signupButton.value = "Returning User";
+		   submitButton.value = "Create Account";
+		}else{
+		   formAction.textContent = 'login';
+		   formTitle.textContent = 'Login';
+		   signupButton.value = "Sign-Up!";
+		   submitButton.value = "Login";
+		}
+		
+		//Fix size, ext
+		repaint(signupButton);
+		repaint(submitButton);
+	}
+	
+	//Helper function, makes browser re-draw to fix CSS re-size issues
+	function repaint(elem){
+		elem.style.display = 'none';
+		elem.style.display = 'inherit';
+	}	
+</script>
 </body>
 </html>
